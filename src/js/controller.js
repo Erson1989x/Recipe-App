ad62ee42f5d0a070442d476b57caefc73029074f
Erhard Nagy <Erson1989x@gmail.com>
2024-11-06 16:18:01 +0200

    in-progress

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,16 +1,22 @@
 import * as model from './model.js';
 import recipeView from './views/reciepeView.js';
 import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
+if(module.hot) {
+  module.hot.accept();
+}
 
 const controlRecipes = async () => {
   try {
+
     const id = window.location.hash.slice(1);
 
     if (!id) return;
+
     recipeView.renderSpinner();
 
     // Loading recipe
@@ -25,6 +31,8 @@ const controlRecipes = async () => {
 
 const controlSearchResults = async () => {
   try {
+    resultsView.renderSpinner();
+
     // 1. Get search query
     const query = searchView.getQuery();
     if (!query) return;
@@ -33,10 +41,9 @@ const controlSearchResults = async () => {
     await model.loadSearchResults(query);
 
     // 3. Render results
-    console.log( model.state.search.results);
-    
+    resultsView.render(model.state.search.results);
   } catch (err) {
-    recipeView.renderError();
+    console.log(err);
   } 
 };
 
